39096fc1f343fd166ab794e3a336f751f8401182
hendrikhoffmann <hhoffmann@web.de>
2017-12-17 13:11:11 +0100

    add initial routing

--- smartfblib.go
+++ smartfblib.go
@@ -1,5 +1,32 @@
 package smartfblib
-
-func SwitchOn() string {
-	return "ToDoFernseheran!"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+func SwitchOn(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Switched on")
+}
+func SwitchOff(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Switched on")
+}
+func SwitchChannel(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var channel string
+	channel = params["channel"]
+	if channel != ""{
+		fmt.Fprintf(w, "Switched to channel %d", channel)		
+	} else {
+		fmt.Fprintf(w, "Channel not found or invalid %d", channel)
+	}
+}
+func SetVolume(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var volume string
+	volume = params["volume"]
+	if volume != ""{
+		fmt.Fprintf(w, "Volume set to %d", volume)		
+	} else {
+		fmt.Fprintf(w, "Volume Parameter not found or invalid %d", volume)
+	}
 }
